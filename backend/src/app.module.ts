b24cebd16f6674d4dc71e0d7bc51c5b46488897e
Joe Potentier <joseph.potentier@gmail.com>
2020-10-07 10:51:11 -0600

    Remove redundent config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
 import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ConfigModule, DbModule],
+  imports: [DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
