796ca8ec078a7b62176600ef1e68a4b84cd34c78
Taylor Zowtuk <zowtukt@gmail.com>
2020-10-08 17:52:46 -0600

    Refactor Recording into a function declaration component to add multiple players

--- frontend/src/components/Recording.page.tsx
+++ frontend/src/components/Recording.page.tsx
@@ -4,16 +4,21 @@ import Button from '@material-ui/core/Button';
 import Player from './Player';
 
 
-const Recording = () => (
+function Recording() {
+  const players = [];
+  for (var i = 0; i < 6; i++) { // 5 players + 1 goalie on field
+    players.push(<Player />);
+  }
+  return (
     <div className='recording'>
       <h1>Recoding Interface</h1>
       <p>Here you will record</p>
-      <Player />
+      {players}
       <Link to="/">
         <Button variant="contained">Dashboard</Button>
       </Link>
     </div>
-    
-  );
+  )
+}
 
 export default Recording;
