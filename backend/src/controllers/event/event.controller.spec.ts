fc50876fc70ac564539f38e0245cf8e99a0a140c
Ayub Ahmed <ayub@ualberta.ca>
2020-10-14 13:28:32 -0600

    fix controller unit test

--- backend/src/controllers/event/event.controller.spec.ts
+++ backend/src/controllers/event/event.controller.spec.ts
@@ -1,24 +1,29 @@
 import { TestingModule, Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Goal } from "src/db/entities/events/goal.entity";
 import { GoalDTO } from "src/dto/events/goal.dto";
 import { GoalService } from "src/services/goal/goal.service";
+import { Repository } from "typeorm";
 import { EventController } from "./event.controller";
 
 
 describe('EventController', () => {
   let controller: EventController;
   let goalService: GoalService;
+  let goalRepository: Repository<Goal>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventController],
       providers: [
-        GoalService
+        GoalService,
+        {provide: 'GoalRepository', useClass: Repository}
       ]
     }).compile();
 
     controller = module.get<EventController>(EventController);
     goalService = module.get<GoalService>(GoalService);
-
+    goalRepository = module.get<Repository<Goal>>(getRepositoryToken(Goal));
   });
 
   it('should be defined', () => {
@@ -27,8 +32,8 @@ describe('EventController', () => {
 
   it('should call event service to create goal event', () => {
     var goal: GoalDTO = new GoalDTO();
-    jest.spyOn(goalService, 'saveGoal').mockImplementation((goal) => { return new Promise<void>(() => {}) });
+    const spy = jest.spyOn(goalService, 'saveGoal').mockImplementation((goal) => {return new Promise<void>(() => {})});
     controller.saveGoalEvent(goal);
-    expect(goalService).toBeCalledTimes(1);
+    expect(spy).toBeCalledTimes(1);
   });
 });
