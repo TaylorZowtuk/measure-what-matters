f01c16fdda10ecbc990f44f4b8ec5cc8a4504202
Ayub Ahmed <ayub@ualberta.ca>
2020-10-15 17:37:30 -0600

    uni tests

--- backend/src/controllers/event/event.controller.spec.ts
+++ backend/src/controllers/event/event.controller.spec.ts
@@ -6,6 +6,18 @@ import { GoalService } from "src/services/goal/goal.service";
 import { Repository } from "typeorm";
 import { EventController } from "./event.controller";
 
+const goalDtos: GoalDTO[] = [
+  {
+    matchId: 1,
+    playerId: 1,
+    time: 200
+  },
+  {
+    matchId: 1,
+    playerId: 1,
+    time: 500
+  }
+];
 
 describe('EventController', () => {
   let controller: EventController;
@@ -30,10 +42,25 @@ describe('EventController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should call event service to create goal event', () => {
+  it('should call goal service to create goal event', () => {
     var goal: GoalDTO = new GoalDTO();
     const spy = jest.spyOn(goalService, 'saveGoal').mockImplementation((goal) => {return new Promise<void>(() => {})});
     controller.saveGoalEvent(goal);
     expect(spy).toBeCalledTimes(1);
   });
+
+  it('should call goal service to get goals by player id', () => {
+    const playerId = 1;
+    jest.spyOn(goalService, 'getGoalsByPlayerId').mockResolvedValue(goalDtos)
+    var response = controller.getGoalByPlayer(playerId);
+    expect(response).resolves.toBe(goalDtos);
+  });
+
+  it('should call goal service to get goals by match id', () => {
+    const matchId = 1;
+    jest.spyOn(goalService, 'getGoalsByMatchId').mockResolvedValue(goalDtos)
+    var response = controller.getGoalByMatch(matchId);
+    expect(response).resolves.toBe(goalDtos);
+  });
+
 });
--- backend/src/services/goal/goal.service.spec.ts
+++ backend/src/services/goal/goal.service.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Goal } from 'src/db/entities/events/goal.entity';
+import { GoalDTO } from 'src/dto/events/goal.dto';
+import { Repository } from 'typeorm';
 import { GoalService } from './goal.service';
+
+const goalDto: GoalDTO = {
+  matchId: 1,
+  playerId: 1,
+  time: 200
+}
+
+const goalDtos: GoalDTO[] = [
+  goalDto
+];
+
+const goal = new Goal();
+goal.playerId = 1;
+goal.matchId = 1;
+goal.time = 200;
+
+const goalEntities: Goal[] = [
+  goal
+];
 
 describe('GoalService', () => {
   let service: GoalService;
+  let goalRepository: Repository<Goal>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GoalService, ],
+      providers: [
+      GoalService, 
+      { provide: 'GoalRepository', useClass: Repository}
+    ],
     }).compile();
 
     service = module.get<GoalService>(GoalService);
+    goalRepository = module.get<Repository<Goal>>(getRepositoryToken(Goal));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should add goal event to db', () => {
+    const spy = jest.spyOn(goalRepository, 'save');
+    service.saveGoal(goalDto);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should get goals with specified player id', () => {
+    jest.spyOn(goalRepository, 'find').mockResolvedValue(goalEntities);
+    var goals = service.getGoalsByPlayerId(goal.playerId);
+    expect(goals).resolves.toBe(goalDtos);
+  });
+
+  it('should get goals with specified matchId', () => {
+    jest.spyOn(goalRepository, 'find').mockResolvedValue(goalEntities);
+    var goals = service.getGoalsByPlayerId(goal.matchId);
+    expect(goals).resolves.toBe(goalDtos);
+  });
+
 });
