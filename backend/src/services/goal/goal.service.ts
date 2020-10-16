983e08cd0ce261cf92de94f69b8602c749510c4d
Ayub Ahmed <ayub@ualberta.ca>
2020-10-15 18:11:17 -0600

    Added code comments

--- backend/src/services/goal/goal.service.ts
+++ backend/src/services/goal/goal.service.ts
@@ -13,20 +13,44 @@ export class GoalService {
         this.goalRepo = goalRepo;
     }
 
+    /**
+     * Saves a goal event to the DB.
+     * 
+     * @param goal - The goal we want to save to the DB
+     */
     async saveGoal(goal: GoalDTO): Promise<void> {
         return this.goalRepo.save(goal);
     }
 
+    /**
+     * Retrieves a list of goals scored by the player.
+     * 
+     * @param playerId - The players id which we want to list goals for
+     * 
+     * @returns A list of goals in a resolved promise
+     */
     async getGoalsByPlayerId(playerId: number): Promise<GoalDTO[]> {
         var goals:any[] = await this.goalRepo.find({where: {playerId: playerId}})
         return this.convertToDto(goals);
     }
 
+    /**
+     * Retrieves a list of goals scored in a match.
+     * 
+     * @param matchId - The match id which we want to list goals for
+     */
     async getGoalsByMatchId(matchId: number): Promise<GoalDTO[]> {
         var goals:any[] = await this.goalRepo.find({where: {matchId: matchId}})
         return this.convertToDto(goals);
     }
 
+    /**
+     * Converts a list of goal entities to a list of goal dtos
+     * 
+     * @param goals - The list of goal entities we want to convert to a list of goal DTOs
+     * 
+     * @returns A list of goal dtos converted from an entity
+     */
     private convertToDto(goals:any[]) {
         var goalDtos: GoalDTO[] = [];
         goals.forEach(element => {
