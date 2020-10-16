d819f0bf048fd9287d5a6daa7b1be4e1d754e5b2
Joe Potentier <j.potentier@ghostlab.ca>
2020-10-15 19:49:35 -0600

    Refactor module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { EventModule } from './event/event.module';
 
 @Module({
   imports: [DbModule, EventModule],
-  controllers: [],
-  providers: [AppService],
 })
 export class AppModule {}
