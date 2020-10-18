9e57c9c402986580e7b5cbb2e998b32f2cb7eae3
bsamimi1 <bsamimi@ualberta.ca>
2020-10-18 13:27:26 -0600

    adding swagger components

--- backend/src/playerStats/playerStats.controller.ts
+++ backend/src/playerStats/playerStats.controller.ts
@@ -17,14 +17,20 @@ export class PlayerStatsController {
     @Get('timeOnField')
     @ApiResponse({
         status: 200,
-        type: Number,
-        isArray: false,
+        type: PlayerTimeDTO,
+        isArray: true,
         description: 'Returns an array of time on field for all players in a specific match',
       })
     async getTimeOnField(@Query('matchId') matchId:number ): Promise<PlayerTimeDTO[]>{
         return await this.playerStatsService.getPlayersTimes(matchId);
     }
 
+    @ApiResponse({
+      status: 201,
+      type: PlayerDTO,
+      isArray: true,
+      description: 'Creates the starting lineup substitutions'
+    })
     @Get('onForGoal')
     async getPlayersOnForGoal(@Query('goalId') goalId:number): Promise<PlayerDTO[]>{
         return await this.playerStatsService.getPlayersOnForGoal(goalId);
