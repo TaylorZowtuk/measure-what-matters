8d00603f5d1a8117d1aef121ffefd026fd58232d
Joe Potentier <j.potentier@ghostlab.ca>
2020-10-22 19:04:51 -0600

    snake -> camel

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     const payload = { username: user.username, sub: user.userId };
     return {
       username: user.username,
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 }
