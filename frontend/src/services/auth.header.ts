0a600a0b4aa277230c75aa4be6af4194d6f4d847
Taylor Zowtuk <zowtukt@gmail.com>
2020-10-25 20:29:11 -0600

    Reduce nested conditionals

--- frontend/src/services/auth.header.ts
+++ frontend/src/services/auth.header.ts
@@ -1,15 +1,12 @@
 export default function authHeader() {
     const json_user = localStorage.getItem("user");
-    let user;
-    console.log(json_user)
-    if (json_user) { 
-        user = JSON.parse(json_user);
-
-        if (user && user.accessToken) {
-            return { Authorization: 'Bearer ' + user.accessToken };
-        }
-    }
-    else {
+    
+    if (!json_user) {
         return {};
     }
+    const user = JSON.parse(json_user);
+    if(!user || !user.accessToken) {
+        return {};
+    }
+    return { Authorization: 'Bearer ' + user.accessToken };
   }
