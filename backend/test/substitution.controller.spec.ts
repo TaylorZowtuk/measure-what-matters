38d377bcca8c96cb8cf4df4fc8889478eee2d175
Ayub Ahmed <ayub@ualberta.ca>
2020-10-26 09:51:00 -0600

    unit tests for controller

--- backend/test/substitution.controller.spec.ts
+++ backend/test/substitution.controller.spec.ts
@@ -1,9 +1,38 @@
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { SubstitutionExchangeDTO } from '../src/dto/events/substitution/substitution-exchange.dto';
 import { SubstitutionDTO } from '../src/dto/events/substitution/substitution.dto';
 import { SubstitutionController } from '../src/event/substitution/substitution.controller';
 import { SubstitutionService } from '../src/event/substitution/substitution.service';
+
+const subExchangeDto: SubstitutionExchangeDTO = {
+  playerIdIn: 1,
+  playerIdOut: 2,
+  time: 453345,
+  matchId: 1,
+};
+
+const subs: SubstitutionDTO[] = [
+  {
+    playerId: 1,
+    matchId: 1,
+    timeOn: 5999,
+  },
+  {
+    playerId: 2,
+    matchId: 1,
+    timeOn: 5999,
+  },
+  {
+    playerId: 3,
+    matchId: 1,
+    timeOn: 5999,
+  },
+];
 
 describe('SubstitutionController', () => {
   let controller: SubstitutionController;
@@ -14,8 +43,8 @@ describe('SubstitutionController', () => {
       controllers: [SubstitutionController],
       providers: [
         SubstitutionService,
-        { provide: 'SubstitutionRepository', useClass: Repository }
-      ]
+        { provide: 'SubstitutionRepository', useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<SubstitutionController>(SubstitutionController);
@@ -26,39 +55,69 @@ describe('SubstitutionController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should call sub service to create starting substitutions', () => {
-    const subs: SubstitutionDTO[] = [
-      {
-        playerId: 1,
-        matchId: 1,
-        timeOn: 5999
-      },
-      {
-        playerId: 2,
-        matchId: 1,
-        timeOn: 5999
-      },
-      {
-        playerId: 3,
-        matchId: 1,
-        timeOn: 5999
-      }
-    ]
-    const spy = jest.spyOn(substitutionService, 'createStartingSubs').mockResolvedValue(undefined);
-    controller.createStartingSubs(subs);
+  it('should call sub service to create starting substitutions', async () => {
+    const spy = jest
+      .spyOn(substitutionService, 'createStartingSubs')
+      .mockResolvedValue(undefined);
+    await controller.createStartingSubs(subs);
     expect(spy).toHaveBeenCalledWith(subs);
   });
 
-  it('should call sub service to create a substitution event', () => {
-    const subExchangeDto: SubstitutionExchangeDTO = {
-      playerIdIn: 1,
-      playerIdOut: 2,
-      time: 453345,
-      matchId: 1
-    }
-    const spy = jest.spyOn(substitutionService, 'saveSubstitution').mockResolvedValue(undefined);
-    controller.saveSubstitutionEvent(subExchangeDto);
+  it('should call sub service to create a substitution event', async () => {
+    const spy = jest
+      .spyOn(substitutionService, 'saveSubstitution')
+      .mockResolvedValue(undefined);
+    await controller.saveSubstitutionEvent(subExchangeDto);
     expect(spy).toHaveBeenCalledWith(subExchangeDto);
   });
 
+  it('should return bad request if not null violation', async () => {
+    const error: QueryFailedError = new QueryFailedError('', [], '');
+    error.message = 'violates not-null constraint';
+    jest
+      .spyOn(substitutionService, 'saveSubstitution')
+      .mockRejectedValue(error);
+    jest
+      .spyOn(substitutionService, 'createStartingSubs')
+      .mockRejectedValue(error);
+    await expect(controller.createStartingSubs(subs)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(
+      controller.saveSubstitutionEvent(subExchangeDto),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('should return bad request if foreign key violation', async () => {
+    const error: QueryFailedError = new QueryFailedError('', [], '');
+    error.message = 'violates foreign key constraint';
+    jest
+      .spyOn(substitutionService, 'saveSubstitution')
+      .mockRejectedValue(error);
+    jest
+      .spyOn(substitutionService, 'createStartingSubs')
+      .mockRejectedValue(error);
+    await expect(controller.createStartingSubs(subs)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(
+      controller.saveSubstitutionEvent(subExchangeDto),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('should return internal server err for other errors', async () => {
+    const error: Error = new Error();
+    jest
+      .spyOn(substitutionService, 'saveSubstitution')
+      .mockRejectedValue(error);
+    jest
+      .spyOn(substitutionService, 'createStartingSubs')
+      .mockRejectedValue(error);
+    await expect(controller.createStartingSubs(subs)).rejects.toThrow(
+      InternalServerErrorException,
+    );
+    await expect(
+      controller.saveSubstitutionEvent(subExchangeDto),
+    ).rejects.toThrow(InternalServerErrorException);
+  });
 });
