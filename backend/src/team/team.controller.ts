1f35b223d4f11327e5d0ac5192b0ca6cc32cdbce
Ayub Ahmed <ayub@ualberta.ca>
2020-10-30 22:28:48 -0600

    err handling, remove uneeded method

--- backend/src/team/team.controller.ts
+++ backend/src/team/team.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Headers,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { QueryFailedError } from 'typeorm';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { TeamDTO } from '../dto/team.dto';
+import { TeamDTO } from '../dto/team/team.dto';
 import { TeamService } from './team.service';
 
 @Controller('teams')
@@ -16,28 +26,37 @@ export class TeamController {
   }
 
   @Post('/')
-  async createTeam(@Body() team: TeamDTO) {
-    this.teamService.saveTeam(team);
+  async createTeam(@Body() team: TeamDTO, @Headers('userId') userId: number) {
+    try {
+      await this.teamService.saveTeam(team, userId);
+    } catch (err) {
+      if (err instanceof QueryFailedError) {
+        if (err.message.includes('violates foreign key constraint')) {
+          throw new BadRequestException(
+            'The request body contains an invalid userId',
+          );
+        } else if (err.message.includes('violates not-null constraint')) {
+          throw new BadRequestException(
+            'The request body contains a missing or null value',
+          );
+        } else if (err.message.includes('violates unique constraint')) {
+          throw new BadRequestException('The team name is already taken');
+        }
+      }
+      throw new InternalServerErrorException(
+        'Something went wrong. Please try again later.',
+      );
+    }
   }
 
-  @Get('/userId')
+  @Get('/')
   @ApiResponse({
     status: 200,
     type: TeamDTO,
     isArray: true,
-    description: 'Gets all teams for the given user',
+    description: 'Gets all teams for the user',
   })
-  async getTeamsByUserId(@Query('userId') userId: number) {
-    return this.teamService.getTeamsByUserId(userId);
-  }
-
-  @Get('/name')
-  @ApiResponse({
-    status: 200,
-    type: TeamDTO,
-    description: 'Gets the team with the given name',
-  })
-  async getTeamByName(@Query('name') name: string) {
-    return this.teamService.getTeamByName(name);
+  async getTeamsByUserId(@Headers('userId') userId: number) {
+    return await this.teamService.getTeamsByUserId(userId);
   }
 }
