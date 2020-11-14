5ec9cd2da222da52c4c2b5f8bbf30b665c0f407d
Ayub Ahmed <ayub@ualberta.ca>
2020-11-13 22:47:47 -0700

    move page into folder

--- frontend/src/components/Lineup/Lineup.page.tsx
+++ frontend/src/components/Lineup/Lineup.page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import authHeader from "../services/auth.header";
+import authHeader from "../../services/auth.header";
 import {
   Button,
   Checkbox,
