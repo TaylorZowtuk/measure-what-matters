a50d108294a10ed5515daa36b72a67a16755558f
LiShang99 <42879014+LiShang99@users.noreply.github.com>
2020-11-14 22:23:05 -0700

    cleaned up code

--- frontend/src/components/Teams/Teams.page.tsx
+++ frontend/src/components/Teams/Teams.page.tsx
@@ -4,7 +4,6 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import authHeader from "../../services/auth.header";
-// import AuthService from "../../services/auth.service";
 import { Container, Row, Col } from "react-bootstrap";
 
 interface teamState {
@@ -24,101 +23,72 @@ interface Player {
   playerId: number;
 }
 
-// const currentUser = AuthService.getCurrentUser();
-
 class Teams extends React.Component<{}, teamState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      teamList: []
-    }
-    // TODO: make request to retrieve team list
-    axios.get(`/teams`, { headers: authHeader() })
-    .then(response => {
-      console.log("got teams");
-      console.log(response.data);
-      let teams = response.data;
-      let teamArray: Team[] = [];
+      teamList: [],
+    };
+    axios.get(`/teams`, { headers: authHeader() }).then(
+      (response) => {
+        console.log("got teams");
+        console.log(response.data);
+        let teams = response.data;
+        let teamArray: Team[] = [];
 
-      teams.forEach((element: any) => {
-        axios.get(`/players/teamId?teamId=${element.teamId}`, { headers: authHeader() })
-        .then(response => {
-          let players: Array<Player> = [];
-          if(response.data) 
-            {response.data.forEach((player: any) => {
-              players.push({firstName: player.firstName, lastName: player.lastName, number: player.jerseyNum, playerId: player.playerId});
-            });}
-          teamArray.push({teamName: element.name, playerList: players, teamId: element.teamId});
-          this.setState({teamList: teamArray});
-        },
-        (error) => {
-          console.log("getting errors");
-          console.log(error);
+        teams.forEach((element: any) => {
+          axios
+            .get(`/players/teamId?teamId=${element.teamId}`, {
+              headers: authHeader(),
+            })
+            .then(
+              (response) => {
+                let players: Array<Player> = [];
+                if (response.data) {
+                  response.data.forEach((player: any) => {
+                    players.push({
+                      firstName: player.firstName,
+                      lastName: player.lastName,
+                      number: player.jerseyNum,
+                      playerId: player.playerId,
+                    });
+                  });
+                }
+                teamArray.push({
+                  teamName: element.name,
+                  playerList: players,
+                  teamId: element.teamId,
+                });
+                this.setState({ teamList: teamArray });
+              },
+              (error) => {
+                console.log("getting errors");
+                console.log(error);
+              }
+            );
         });
-      });
-    },
-    (error) => {
-      console.log("getting errors");
-      console.log(localStorage.getItem("user"));
-    })
-
-  //   this.state = {
-  //     teamList: [
-  //       {
-  //         teamName: "Boys Team",
-  //         playerList: [
-  //           {
-  //             name: "John",
-  //             number: 1,
-  //           },
-  //           {
-  //             name: "Tom",
-  //             number: 2,
-  //           },
-  //           {
-  //             name: "Bob",
-  //             number: 3,
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         teamName: "Girls Team",
-  //         playerList: [
-  //           {
-  //             name: "Cat",
-  //             number: 5,
-  //           },
-  //           {
-  //             name: "Lily",
-  //             number: 6,
-  //           },
-  //           {
-  //             name: "Mag",
-  //             number: 8,
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   };
+      },
+      (error) => {
+        console.log("getting errors");
+        console.log(localStorage.getItem("user"));
+      }
+    );
   }
 
   public render() {
     return (
       <Container>
         <h3>Your Teams</h3>
-        {
-        this.state.teamList.map((team) => {
+        {this.state.teamList.map((team) => {
           return (
             <Row key={team.teamId.toString()}>
               <Col>
                 {team.teamName}
                 <TeamComponent playerList={team.playerList}></TeamComponent>
-                {/* <Button variant="contained">Edit Team</Button> */}
               </Col>
             </Row>
           );
-        })
-        }
+        })}
         <Link to="/create-team">
           <Button variant="contained" style={{ marginBottom: 10 }}>
             Add Team
