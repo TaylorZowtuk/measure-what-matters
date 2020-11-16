e36f98fe6f4de0c6fa1f6a4f331f0e3339c3e5d0
Joe Potentier <j.potentier@ghostlab.ca>
2020-11-15 17:10:01 -0700

    WIP - more tests

--- backend/test/match.service.spec.ts
+++ backend/test/match.service.spec.ts
@@ -4,30 +4,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MatchService } from '../src/match/match.service';
 import { Match } from '../src/db/entities/match.entity';
 import { CreateMatchDTO } from '../src/dto/match/createMatch.dto';
+import { CreateUserDTO } from '../src/dto/users/createUser.dto';
+import { repositoryMockFactory } from './mocks/repositoryMockFactory';
+import { MockType } from './mocks/mockType';
+import { StartMatchDTO } from '../src/dto/match/startMatch.dto';
+import { Team } from '../src/db/entities/team.entity';
+import { User } from '../src/db/entities/user.entity';
 
 describe('MatchService Test', () => {
+  const matchRepoToken = getRepositoryToken(Match);
+  let matchRepo: MockType<Repository<Match>>;
   let matchService: MatchService;
-  let matchRepository: Repository<Match>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MatchService,
-        { provide: 'MatchRepository', useClass: Repository },
+        { provide: matchRepoToken, useFactory: repositoryMockFactory },
       ],
     }).compile();
 
     matchService = module.get<MatchService>(MatchService);
-    matchRepository = module.get<Repository<Match>>(getRepositoryToken(Match));
+    matchRepo = module.get<MockType<Repository<Match>>>(matchRepoToken);
   });
 
   // // TODO, check these tests. They were passing locally and failing in the pipeline. Fixed the type errors now.
-  // const createMatchDto: CreateMatchDTO = {
-  //   teamId: 1,
-  //   scheduledTime: 100,
-  //   isHomeTeam: true,
-  //   opponentTeamName: 'bad team name',
-  // };
 
   // const match = new Match();
 
@@ -38,31 +39,129 @@ describe('MatchService Test', () => {
   // // const matchDtos: CreateMatchDTO[] = [createMatchDto, createMatchDto];
   // const matchEntities: Match[] = [match, match];
 
+  const user: User = {
+    "userId": 1,
+    "name": "t",
+    "username": "t",
+    "password": "$2b$10$L.4Wzh5WMcUSnfhVQUZ2ZulXSLRGrT8v0dlA6KU86yzUKtJ.1SFni",
+    "createdDate": new Date(),
+    "updatedDate": new Date()
+  }
+
+  const team: Team = {
+    "teamId": 1,
+    "name": "team",
+    "userId": 1,
+    "createdDate": "2020-11-15T09:19:45.170Z",
+    "updatedDate": "2020-11-15T09:19:45.170Z",
+    "user": {
+      "userId": 1,
+      "name": "t",
+      "username": "t",
+      "password": "$2b$10$L.4Wzh5WMcUSnfhVQUZ2ZulXSLRGrT8v0dlA6KU86yzUKtJ.1SFni",
+      "createdDate": "2020-11-15T09:19:45.164Z",
+      "updatedDate": "2020-11-15T09:19:45.164Z"
+    }
+
+  const match: Match = {
+    "matchId": 1,
+    "teamId": 1,
+    "scheduledTime": 46544654,
+    "startTime": 255,
+    "halfTime": 66,
+    "fullTime": null,
+    "opponentTeamName": "46544655",
+    "isHomeTeam": true,
+    "createdDate": "2020-11-15T09:19:45.172Z",
+    "updatedDate": "2020-11-16T07:06:22.929Z",
+    "team": {
+      "teamId": 1,
+      "name": "team",
+      "userId": 1,
+      "createdDate": "2020-11-15T09:19:45.170Z",
+      "updatedDate": "2020-11-15T09:19:45.170Z",
+      "user": {
+        "userId": 1,
+        "name": "t",
+        "username": "t",
+        "password": "$2b$10$L.4Wzh5WMcUSnfhVQUZ2ZulXSLRGrT8v0dlA6KU86yzUKtJ.1SFni",
+        "createdDate": "2020-11-15T09:19:45.164Z",
+        "updatedDate": "2020-11-15T09:19:45.164Z"
+      }
+    }
+  }
+
   it('check if service defined', () => {
     expect(matchService).toBeDefined();
-    expect(matchRepository).toBeDefined();
   });
 
-  // describe('Saving Matches', () => {
-  //   const { matchId } = createMatchDto;
-  //   it('should call save 1 times', async () => {
-  //     const spy = jest.spyOn(matchRepository, 'save').mockResolvedValue(match);
-  //     await matchService.startMatch();
-  //     expect(spy).toBeCalledTimes(1);
-  //   });
-  // });
+  describe('Scheduling a match', () => {
+    const createMatchDto: CreateMatchDTO = {
+      teamId: 1,
+      scheduledTime: 100,
+      isHomeTeam: true,
+      opponentTeamName: 'bad team name',
+    };
+    it('should call create on match repo once with all required not-null values of a match', async () => {
+      const {
+        teamId,
+        scheduledTime,
+        opponentTeamName,
+        isHomeTeam,
+      } = createMatchDto;
 
-  // describe('Getting Matches', () => {
-  //   it('matchRepository find method should be called using teamId', async () => {
-  //     const teamId = 1;
-  //     const spy = jest
-  //       .spyOn(matchRepository, 'find')
-  //       .mockResolvedValueOnce(matchEntities);
+      await matchService.createMatch(
+        teamId,
+        scheduledTime,
+        opponentTeamName,
+        isHomeTeam,
+      );
 
-  //     await matchService.getMatches(teamId);
+      expect(matchRepo.create).toBeCalledTimes(1);
+    });
 
-  //     expect(spy).toBeCalledTimes(1);
-  //     expect(spy).toBeCalledWith({ where: { teamId } });
-  //   });
-  // });
+    it('repo save should be called with an object of Match type', async () => {
+      const {
+        teamId,
+        scheduledTime,
+        opponentTeamName,
+        isHomeTeam,
+      } = createMatchDto;
+
+      const expected = matchRepo.create({
+        teamId,
+        scheduledTime,
+        opponentTeamName,
+        isHomeTeam,
+      });
+
+      await matchService.createMatch(
+        teamId,
+        scheduledTime,
+        opponentTeamName,
+        isHomeTeam,
+      );
+
+      expect(matchRepo.save).toBeCalledTimes(1);
+      expect(matchRepo.save).toBeCalledWith(expected);
+    });
+  });
+
+  describe('Starting a match', () => {
+    const startMatchDTO: StartMatchDTO = {
+      matchId: 1,
+      time: 565,
+    };
+    it('should throw an error if matchId is null', async () => {
+      const { time } = startMatchDTO;
+      const badMatchId = null;
+      matchRepo.findOneOrFail.mockResolvedValueOnce()
+
+      await matchService.startMatch(badMatchId, time);
+    });
+
+    it('should call a find method with matchId', async () => {});
+
+    it('should call save with startTime defined', async () => {});
+  });
 });
