17b82412314ac4af8bd6af1b297a9b8ca932cc94
Taylor Zowtuk <zowtukt@gmail.com>
2020-11-15 17:09:53 -0700

    Handle player dto changes

--- frontend/src/components/interfaces/player.tsx
+++ frontend/src/components/interfaces/player.tsx
@@ -1,9 +1,12 @@
 interface Player {
+  playerId: number; // Unique player id from the db
+  teamId: number; // Unique team id from the db
   firstName: string;
   lastName: string;
   jerseyNum: number; // Jersey number
-  teamId: number; // Unique team id from the db
-  playerId: number; // Unique player id from the db
+  archived: boolean;
+  createdDate: string;
+  updatedDate: string;
 }
 
 export default Player;
