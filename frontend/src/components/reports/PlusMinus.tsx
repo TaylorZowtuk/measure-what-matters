2dfc9ea55a7de35b2fd77f4e10d4ef6100d0a272
Ayub Ahmed <ayub@ualberta.ca>
2020-11-17 01:22:23 -0700

    styling and cleanup

--- frontend/src/components/reports/PlusMinus.tsx
+++ frontend/src/components/reports/PlusMinus.tsx
@@ -18,6 +18,7 @@ import {
   Theme,
 } from "@material-ui/core";
 import ReportProps from "../interfaces/props/report-props";
+import CSS from "csstype";
 
 const useToolbarStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -57,6 +58,12 @@ const EnhancedTableToolbar = () => {
   );
 };
 
+const tableStyling: CSS.Properties = {
+  height: "50vh",
+  width: "30vw",
+  margin: "auto",
+};
+
 async function fetchPlusMinus(matchId: number): Promise<PlusMinus[]> {
   const response = await axios.get(
     `/player-stats/plus-minus?matchId=${matchId}`,
@@ -65,6 +72,28 @@ async function fetchPlusMinus(matchId: number): Promise<PlusMinus[]> {
     }
   );
   return response.data;
+}
+
+function getTableFrame(tableBody: JSX.Element | null): JSX.Element {
+  return (
+    <div className="PlusMinus">
+      <TableContainer component={Paper} style={tableStyling}>
+        <EnhancedTableToolbar />
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell align="center">Jersey Number</TableCell>
+              <TableCell align="center">Plus Minus</TableCell>
+            </TableRow>
+          </TableHead>
+          {tableBody}
+          {/* Adds the table body to a table frame just to avoid repeating */}
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default function PlusMinusComponent(props: ReportProps) {
@@ -80,71 +109,32 @@ export default function PlusMinusComponent(props: ReportProps) {
   }, [props]);
 
   if (!plusMinus || !props.matchId) {
-    return (
-      <div className="PlusMinus">
-        <TableContainer component={Paper}>
-          <EnhancedTableToolbar />
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell>First Name</TableCell>
-                <TableCell>Last Name</TableCell>
-                <TableCell align="center">Jersey Number</TableCell>
-                <TableCell align="center">Plus Minus</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableCell />
-              <TableCell />
-              <TableCell />
-              <TableCell />
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    );
+    return getTableFrame(null); // Pass null since we don't have the data to render the table
   } else {
-    return (
-      <div className="PlusMinus">
-        <TableContainer component={Paper}>
-          <EnhancedTableToolbar />
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell>First Name</TableCell>
-                <TableCell>Last Name</TableCell>
-                <TableCell align="center">Jersey Number</TableCell>
-                <TableCell align="center">Plus Minus</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {plusMinus.map((plusMinus) => (
-                <TableRow key={plusMinus.player.playerId}>
-                  <TableCell>{plusMinus.player.firstName}</TableCell>
-                  <TableCell>{plusMinus.player.lastName}</TableCell>
-                  <TableCell align="center">
-                    {plusMinus.player.jerseyNum}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    style={{
-                      backgroundColor:
-                        plusMinus.plusMinus <= 0
-                          ? plusMinus.plusMinus === 0
-                            ? "white"
-                            : "red"
-                          : "green",
-                      color: plusMinus.plusMinus !== 0 ? "white" : "black",
-                    }}
-                  >
-                    {plusMinus.plusMinus}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+    return getTableFrame(
+      <TableBody>
+        {plusMinus.map((plusMinus) => (
+          <TableRow key={plusMinus.player.playerId}>
+            <TableCell>{plusMinus.player.firstName}</TableCell>
+            <TableCell>{plusMinus.player.lastName}</TableCell>
+            <TableCell align="center">{plusMinus.player.jerseyNum}</TableCell>
+            <TableCell
+              align="center"
+              style={{
+                backgroundColor:
+                  plusMinus.plusMinus <= 0
+                    ? plusMinus.plusMinus === 0
+                      ? "white"
+                      : "red"
+                    : "green",
+                color: plusMinus.plusMinus !== 0 ? "white" : "black",
+              }}
+            >
+              {plusMinus.plusMinus}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     );
   }
 }
