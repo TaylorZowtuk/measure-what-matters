ddaa5ffa43bd1970ea42f501322cf2828cc4edca
Joe Potentier <joseph.potentier@gmail.com>
2020-11-21 14:53:09 -0700

    js not json

--- bin/deploy/deploy.sh
+++ bin/deploy/deploy.sh
@@ -4,9 +4,6 @@ PATH=$PATH:/home/ubuntu/n/bin:/home/ubuntu/bin
 TARGET='/home/ubuntu/mwm'
 cd $TARGET
 
-pwd
-whoami
-
 ./bin/nginx/create_lnk.sh && sudo systemctl restart nginx
 
-pm2 reload ecosystem.config.json
+pm2 reload ecosystem.config.js
