0f869753be3dcf054725a9fe7a7fa0f2561d2c22
Joe Potentier <joseph.potentier@gmail.com>
2020-11-21 15:50:03 -0700

    fix env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,18 @@ module.exports = {
     {
       name: 'mwm-backend',
       script: 'backend/dist/main.js',
-      interpreter: '/home/ubuntu/n/n/versions/node/14.15.1/bin/node',
       env: {
         NODE_ENV: 'production',
         APP_PORT: 4019,
         DATABASE_TYPE: 'postgres',
         DATABASE_HOST: 'localhost',
-        DATABASE_PORT: 5984,
+        DATABASE_PORT: 5432,
         DATABASE_NAME: 'mwm',
         DATABASE_USER: 'mwm',
         DATABASE_PASSWORD: 'VOtgTCqCIA3IpLEGqtV6GCVp',
+        JWT_SECRET:
+          'iSTzo8slM5axIjPwZWX1SBltTyxy23B6Wi7nEhnXHm92uRPs5dA9WR2DRih19uU0',
+        JWT_EXPIRES_IN: '10080m',
       },
     },
     {
