b32e4bcbf1a44a01f708182393885f3409b63a41
Ayub Ahmed <ayub@ualberta.ca>
2020-11-22 21:46:48 -0700

    added buttons to nav, default text on dashboard

--- frontend/src/components/AppNavbar.tsx
+++ frontend/src/components/AppNavbar.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import AuthService from "../services/auth.service";
+import CSS from "csstype";
+
+const buttonStyling: CSS.Properties = {
+  margin: "10px",
+};
 
 export default function AppNavbar() {
   return (
@@ -10,12 +15,33 @@ export default function AppNavbar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
+          <Link to="/matches/upcoming">
+            <Button variant="outline-secondary" style={buttonStyling}>
+              Recording
+            </Button>
+          </Link>
+          <Link to="/teams">
+            <Button variant="outline-secondary" style={buttonStyling}>
+              Teams
+            </Button>
+          </Link>
+          <Link to="/create-match">
+            <Button variant="outline-secondary" style={buttonStyling}>
+              Create Match
+            </Button>
+          </Link>
+          <Link to="/view-account">
+            <Button variant="outline-secondary" style={buttonStyling}>
+              Account
+            </Button>
+          </Link>
           <Link to="/">
             <Button
-              variant="outline-secondary"
+              variant="outline-danger"
               onClick={() => {
                 AuthService.logout();
               }}
+              style={buttonStyling}
             >
               Logout
             </Button>
