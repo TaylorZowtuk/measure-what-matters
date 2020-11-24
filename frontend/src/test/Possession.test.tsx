324c2ab84b58ff0f26e0e42b5f7e1ebbbb5a7f5f
Taylor Zowtuk <zowtukt@gmail.com>
2020-11-23 19:31:38 -0700

    Fix possession tests

--- frontend/src/test/Possession.test.tsx
+++ frontend/src/test/Possession.test.tsx
@@ -1,31 +1,37 @@
 import React from "react";
-import {
-  cleanup,
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { cleanup, render, waitForElement } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import PossessionCircular, {
-  CircularProgressWithLabel,
   fetchTimes,
 } from "../components/reports/Possession";
 import { CircularProgress } from "@material-ui/core";
 
 afterEach(cleanup);
 
-// test("renders possession time reports", () => {
-//   const { getByText } = render(<PossessionCircular fetchTimes={() => {}} />);
+test("renders possession time reports", async () => {
+  const mockFetch = jest.fn() as jest.MockedFunction<typeof fetchTimes>;
+  mockFetch.mockReturnValue(
+    new Promise((resolve, reject) => {
+      resolve([0, 31, 100]);
+    })
+  );
 
-//   const headerElement = getByText(/Possession/i);
-//   expect(headerElement).toBeInTheDocument();
-//   const firstHalf = getByText(/first/i);
-//   expect(firstHalf).toBeInTheDocument();
-//   const secondHalf = getByText(/second/i);
-//   expect(secondHalf).toBeInTheDocument();
-//   const thirdHalf = getByText(/overall/i);
-//   expect(thirdHalf).toBeInTheDocument();
-// });
+  const { getByText } = render(
+    <PossessionCircular matchId={1} fetchTimes={mockFetch} />
+  );
+  // While the fetch hasnt completed shows loading
+  await waitForElement(() => <CircularProgress />);
+  expect(mockFetch).toHaveBeenCalled();
+
+  const headerElement = getByText(/Possession/i);
+  expect(headerElement).toBeInTheDocument();
+  const firstHalf = getByText(/first/i);
+  expect(firstHalf).toBeInTheDocument();
+  const secondHalf = getByText(/second/i);
+  expect(secondHalf).toBeInTheDocument();
+  const thirdHalf = getByText(/overall/i);
+  expect(thirdHalf).toBeInTheDocument();
+});
 
 test("renders the percentage of possession times", async () => {
   const mockFetch = jest.fn() as jest.MockedFunction<typeof fetchTimes>;
@@ -38,8 +44,9 @@ test("renders the percentage of possession times", async () => {
   const { getByText } = render(
     <PossessionCircular matchId={1} fetchTimes={mockFetch} />
   );
+  // While the fetch hasnt completed shows loading
+  await waitForElement(() => <CircularProgress />);
   expect(mockFetch).toHaveBeenCalled();
-  await waitForElementToBeRemoved(() => <CircularProgress />);
 
   const firstPossession = getByText("0%");
   expect(firstPossession).toBeInTheDocument();
@@ -47,4 +54,29 @@ test("renders the percentage of possession times", async () => {
   expect(secondPossession).toBeInTheDocument();
   const overallPossession = getByText("100%");
   expect(overallPossession).toBeInTheDocument();
+});
+
+test("renders loading indicator while data fetch hasnt completed", async () => {
+  const mockFetch = jest.fn() as jest.MockedFunction<typeof fetchTimes>;
+  mockFetch.mockReturnValue(
+    new Promise((resolve, reject) => {
+      resolve([0, 31, 100]);
+    })
+  );
+
+  const { getByText } = render(
+    <PossessionCircular matchId={1} fetchTimes={mockFetch} />
+  );
+  // While the fetch hasnt completed shows loading
+  await waitForElement(() => <CircularProgress />);
+  expect(mockFetch).toHaveBeenCalled();
+
+  const headerElement = getByText(/Possession/i);
+  expect(headerElement).toBeInTheDocument();
+  const firstHalf = getByText(/first/i);
+  expect(firstHalf).toBeInTheDocument();
+  const secondHalf = getByText(/second/i);
+  expect(secondHalf).toBeInTheDocument();
+  const thirdHalf = getByText(/overall/i);
+  expect(thirdHalf).toBeInTheDocument();
 });
